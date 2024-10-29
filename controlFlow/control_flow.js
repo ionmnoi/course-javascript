676697e46af58ae5150160e351214c4346a7a1eb
Jon Wilson <ionmnoi@gmail.com>
2024-10-29 16:36:56 -0400

    flow control changes commit

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -4,6 +4,8 @@ let isLoggedIn = true;
 let userMessage;
 let userType = "subscriber";
 let userCategory;
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 if (userRole === "admin") {
     accessLevel = "Full access granted";
@@ -43,3 +45,5 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+console.log("Authentication Status:", authenticationStatus);
